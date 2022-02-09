eb4871e6515b80964497d359109a5d86cfb09320
Nicky Thayer <thayer.nick72@gmail.com>
2022-02-09 17:48:06 -0500

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, 
-    Modal, Button, StyleSheet, 
-    Alert, PanResponder} from 'react-native';
+import { Text, View, ScrollView, FlatList,
+    Modal, Button, StyleSheet,
+    Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { postFavorite, postComment } from '../redux/ActionCreators';
 import { connect } from 'react-redux';
@@ -65,6 +65,16 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    };
+
     if (campsite) {
         return (
             <Animatable.View
@@ -99,6 +109,14 @@ function RenderCampsite(props) {
                     onPress = {() => props.onShowModal()}
                 />
                 </View>
+                <Icon
+                    name={'share'}
+                    type='font-awesome'
+                    color='#5637DD'
+                    raised
+                    reverse
+                    onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
+                        />
                 </Card>
             </Animatable.View>
         );
